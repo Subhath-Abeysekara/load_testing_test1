5930e1715b57026550a8aac5e1564c7d5a86ccb6
jerom <jeromroshane911@gmail.com>
2024-09-12 19:33:25 +0530

    update3

--- index2.js
+++ index2.js
@@ -74,6 +74,41 @@ app.post('/upload', upload.single('file'), (req, res) => {
   res.send(`File uploaded successfully: ${req.file.filename}`);
 });
 
+app.get('/video', (req, res) => {
+  const videoPath = path.join(__dirname,'uploads', '1726148544640-converted.mp4');
+  const videoStat = fs.statSync(videoPath);
+  const fileSize = videoStat.size;
+  const range = req.headers.range;
+
+  if (range) {
+    // Handle range requests for video streaming
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+    const chunkSize = (end - start) + 1;
+    const file = fs.createReadStream(videoPath, { start, end });
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(206, head); // Partial Content
+    file.pipe(res);
+  } else {
+    // If no range header, send the entire video
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    };
+
+    res.writeHead(200, head);
+    fs.createReadStream(videoPath).pipe(res);
+  }
+});
+
 // Start the server
 app.listen(3001, () => {
   console.log('Server is running on port 3001 2');
